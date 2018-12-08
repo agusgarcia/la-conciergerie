966e7193d0b9debc937053ee74ff04840fcbdd2d
Agustina <geminis_agus@hotmail.com>
2018-12-08 11:03:36 +0100

    Unfinished Archive Exhibition Alphabetic sort

--- wp-content/themes/laconciergerie/app/Controllers/ArchiveExhibition.php
+++ wp-content/themes/laconciergerie/app/Controllers/ArchiveExhibition.php
@@ -46,6 +46,7 @@ class ArchiveExhibition extends Controller
                 $post->exhibition_title = get_field('exhibition_title', $post);
                 $post->thumbnail = get_field('thumbnail', $post);
                 $post->color = get_field('color', $post);
+                $post->name = explode(' ', $post->artist_name)[1];
 
                 return $post;
 
@@ -68,8 +69,6 @@ class ArchiveExhibition extends Controller
         $args = array(
             'numberposts' => -1,
             'post_type' => 'exhibition',
-            'order' => 'ASC',
-            'order_by' => 'post_title'
         );
 
         $postsList = get_posts($args);
@@ -87,12 +86,13 @@ class ArchiveExhibition extends Controller
 
         $letter_keyed_posts = array();
 
+
         if ($result) {
             foreach ($result as $post) {
-                $postArray = (array) $post;
+                $postArray = (array)$post;
                 $name = explode(' ', $postArray['artist_name'])[1];
                 $first_letter = strtoupper(substr($name, 0, 1));
-
+                $post->name = $name;
                 // If the array $letter_keyed_posts hasn't a key $first_letter
                 // Create an array into the key
                 if (!array_key_exists($first_letter, $letter_keyed_posts)) {
@@ -102,6 +102,11 @@ class ArchiveExhibition extends Controller
                 $letter_keyed_posts[$first_letter][] = $post;
             }
         }
+        foreach ($letter_keyed_posts as $key => $row) {
+            $letter_keyed_posts[$key] = $row['name'];
+        }
+        array_multisort($letter_keyed_posts, SORT_ASC, $letter_keyed_posts);
+        var_dump($letter_keyed_posts);
         return $letter_keyed_posts;
     }
 
